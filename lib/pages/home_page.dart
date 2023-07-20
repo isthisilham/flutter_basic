5cf7a4d3f8f2842f0d0d5caf6d2354cc7fc79080
isthisilham <muhammadilham6656@gmail.com>
2023-07-20 15:00:04 +0700

    avatar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,6 +21,30 @@ class HomePage extends StatelessWidget {
                     fontSize: 20,
                     fontWeight: FontWeight.w600),
               ),
+              SizedBox(
+                height: 50,
+              ),
+              Image.asset(
+                'assets/images/primary.jpg',
+                width: 140,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Anne Margaritha',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                'UX Designer',
+                style: TextStyle(fontSize: 16, color: greyColor),
+              ),
+              SizedBox(
+                height: 70,
+              )
             ],
           ),
         ),
