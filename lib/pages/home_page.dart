8253793f46d7cf80232322844e22ef647a7b30e9
isthisilham <muhammadilham6656@gmail.com>
2023-07-20 20:48:54 +0700

    scroll view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,94 +10,99 @@ class HomePage extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Profile Picture',
-                style: TextStyle(
-                    color: primaryColor,
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/primary.png',
-                width: 140,
-              ),
-              SizedBox(
-                height: 16,
-              ),
-              Text(
-                'Anne Margaritha',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'UX Designer',
-                style: TextStyle(fontSize: 16, color: greyColor),
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Wrap(
-                spacing: 38,
-                runSpacing: 40,
-                children: [
-                  Image.asset(
-                    'assets/images/item1.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item2.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item3.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item4.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item5.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/item6.png',
-                    width: 80,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 70,
-              ),
-              Container(
-                width: 224,
-                height: 55,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Update Profile',
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: primaryColor),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(16)),
-                      backgroundColor: whiteColor),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
                 ),
-              ),
-            ],
+                Text(
+                  'Profile Picture',
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Image.asset(
+                  'assets/images/primary.png',
+                  width: 140,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Anne Margaritha',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'UX Designer',
+                  style: TextStyle(fontSize: 16, color: greyColor),
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Wrap(
+                  spacing: 38,
+                  runSpacing: 40,
+                  children: [
+                    Image.asset(
+                      'assets/images/item1.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item2.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item3.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item4.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item5.png',
+                      width: 80,
+                    ),
+                    Image.asset(
+                      'assets/images/item6.png',
+                      width: 80,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: primaryColor),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        backgroundColor: whiteColor),
+                  ),
+                ),
+                SizedBox(
+                  height: 76,
+                )
+              ],
+            ),
           ),
         ),
       ),
